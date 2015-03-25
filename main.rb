14108557b834957ee2bbc9eb135fb57499921579
Jack Bush <bush.jack@gmail.com>
2015-03-25 21:55:43 +0000

    commented most of main.rb file back in, added better test values for working without headset. need to get info in routes only returning curent value and repeatedly query, rather than usiong work function

--- main.rb
+++ main.rb
@@ -12,56 +12,51 @@ connection :neurosky, :adaptor => :neurosky, :port => '/dev/tty.MindWave'
 device :neurosky, :driver => :neurosky, :interval => 0.1
 
 def handle_eeg(sender, data)
-#   # content_type :json    
-#   # headers 'Access-Control-Allow-Origin' => '*', 
-#   #         'Access-Control-Allow-Methods' => ['OPTIONS', 'GET', 'POST'],
-#   #         'Access-Control-Allow-Headers' => 'Content-Type' 
-#   # json data
-
   puts "#{sender} | #{data}"
-
-#   data
+  # data
 end
 
-# test = [{thing: 4}]
+test_hash = { delta: 7023617, theta: 15294464, lo_alpha: 15209472, hi_alpha: 13321984, lo_beta: 4527616, hi_beta: 12073472, lo_gamma: 862464, mid_gamma: 13637632 }
+test_range = *(70..79)
+test_num = test_range.sample
 
 get '/' do
   erb :index
 end
 
-# get '/json' do
-#   # content_type :json    
-#   # headers 'Access-Control-Allow-Origin' => '*', 
-#   #         'Access-Control-Allow-Methods' => ['OPTIONS', 'GET', 'POST'],
-#   #         'Access-Control-Allow-Headers' => 'Content-Type' 
-#   # json test
-# end
+get '/json' do
+  # content_type :json    
+  # headers 'Access-Control-Allow-Origin' => '*', 
+  #         'Access-Control-Allow-Methods' => ['OPTIONS', 'GET', 'POST'],
+  #         'Access-Control-Allow-Headers' => 'Content-Type' 
+  # json test
+end
 
-# options '/eeg' do
-#   200
-# end
-# get '/eeg' do
-  # work do
-  #   on neurosky, :eeg => :handle_eeg
-  # end
-# end
+options '/eeg' do
+  200
+end
+get '/eeg' do
+  work do
+    on neurosky, :eeg => :handle_eeg
+  end
+end
 
-# options '/attention' do
-#   200
-# end
+options '/attention' do
+  200
+end
 # get '/attention' do
-  # THIS WORKS AS IS, BUT STOPS WORKING WHEN OUTER FUNCTION IS COMMENTED IN
+  # THIS WORKS ONLY WHEN SEPARATED FROM ROUTE
   work do
     puts "SCANNING ATTENTION..."
     on neurosky, :attention => :handle_eeg
   end
 # end
 
-# options '/meditation' do
-#   200
-# end
-# get '/meditation' do
-#   work do
-#     on neurosky, :meditation => :handle_eeg
-#   end
-# end
+options '/meditation' do
+  200
+end
+get '/meditation' do
+  work do
+    on neurosky, :meditation => :handle_eeg
+  end
+end
