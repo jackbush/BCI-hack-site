a9f59df5e4daab6118432e2eade9e8c30220e27d
Jack Bush <bush.jack@gmail.com>
2015-03-25 21:26:43 +0000

    glacial debug through recommenting file, eeg works as is but sinatra doesn't

--- main.rb
+++ main.rb
@@ -1,60 +1,67 @@
 require 'sinatra'
-# require 'sinatra/contrib/all'
-# require 'sinatra/json'
 require 'json'
-# require 'pry-byebug'
-# require 'pry'
 require 'artoo'
+
+# # GEMS INCOMPATIBLE WITH ARTOO
+# # require 'sinatra/contrib/all'
+# # require 'sinatra/json'
+# # require 'pry-byebug'
+# # require 'pry'
 
 connection :neurosky, :adaptor => :neurosky, :port => '/dev/tty.MindWave'
 device :neurosky, :driver => :neurosky, :interval => 0.1
 
-def handle_feed(sender, data)
-  content_type :json    
-  headers 'Access-Control-Allow-Origin' => '*', 
-          'Access-Control-Allow-Methods' => ['OPTIONS', 'GET', 'POST'],
-          'Access-Control-Allow-Headers' => 'Content-Type' 
-  json data
-  # puts "#{sender} | #{data}"
+def handle_eeg(sender, data)
+#   # content_type :json    
+#   # headers 'Access-Control-Allow-Origin' => '*', 
+#   #         'Access-Control-Allow-Methods' => ['OPTIONS', 'GET', 'POST'],
+#   #         'Access-Control-Allow-Headers' => 'Content-Type' 
+#   # json data
+
+  puts "#{sender} | #{data}"
+
+#   data
 end
 
-test = [{thing: 4}]
+# test = [{thing: 4}]
 
 get '/' do
   erb :index
 end
 
-get '/json' do
-  # content_type :json    
-  # headers 'Access-Control-Allow-Origin' => '*', 
-  #         'Access-Control-Allow-Methods' => ['OPTIONS', 'GET', 'POST'],
-  #         'Access-Control-Allow-Headers' => 'Content-Type' 
-  # json test
-end
+# get '/json' do
+#   # content_type :json    
+#   # headers 'Access-Control-Allow-Origin' => '*', 
+#   #         'Access-Control-Allow-Methods' => ['OPTIONS', 'GET', 'POST'],
+#   #         'Access-Control-Allow-Headers' => 'Content-Type' 
+#   # json test
+# end
 
-options '/eeg' do
-  200
-end
-get '/eeg' do
-  work do
-    on neurosky, :eeg => :handle_feed
-  end
-end
+# options '/eeg' do
+#   200
+# end
+# get '/eeg' do
+  # work do
+  #   on neurosky, :eeg => :handle_eeg
+  # end
+# end
 
-options '/attention' do
-  200
-end
-get '/attention' do
+# options '/attention' do
+#   200
+# end
+# get '/attention' do
+  # THIS WORKS AS IS, BUT STOPS WORKING WHEN OUTER FUNCTION IS COMMENTED IN
   work do
-    on neurosky, :attention => :handle_feed
+    puts "SCANNING ATTENTION..."
+    on neurosky, :attention => :handle_eeg
   end
-end
+# end
 
-options '/meditation' do
-  200
-end
-get '/meditation' do
-  work do
-    on neurosky, :meditation => :handle_feed
-  end
-end
+# options '/meditation' do
+#   200
+# end
+# get '/meditation' do
+#   work do
+#     on neurosky, :meditation => :handle_eeg
+#   end
+# end
