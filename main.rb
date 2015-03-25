48f2778e557cc58fe49e1ebaa943a34d1a57ac7e
Jack Bush <bush.jack@gmail.com>
2015-03-25 20:29:30 +0000

    added attention and meditation feeds. as yet not working

--- main.rb
+++ main.rb
@@ -1,22 +1,23 @@
 require 'sinatra'
-require 'sinatra/contrib/all'
-require 'sinatra/json'
-require 'json'
-require 'pry-byebug'
-require 'pry'
-# require 'artoo'
+# require 'sinatra/contrib/all'
+# require 'sinatra/json'
+# require 'json'
+# require 'pry-byebug'
+# require 'pry'
+require 'artoo'
 
-# connection :neurosky, :adaptor => :neurosky, :port => '/dev/tty.MindWave'
-# device :neurosky, :driver => :neurosky, :interval => 0.1
+connection :neurosky, :adaptor => :neurosky, :port => '/dev/tty.MindWave'
+device :neurosky, :driver => :neurosky, :interval => 0.1
 
-def handle_eeg(sender, data)
+def handle_feed(sender, data)
+  puts sender
   puts data
 end
 
 # eeg = [
 #   # the below creates a feed of brain value hashes
 #   work do
-#     on neurosky, :eeg => :handle_eeg
+#     on neurosky, :eeg => :handle_feed
 #   end
 # ]
 
@@ -30,10 +31,28 @@ options '/eeg' do
   200
 end
 
+get '/json' do
+  # content_type :json    
+  # headers 'Access-Control-Allow-Origin' => '*', 
+  #         'Access-Control-Allow-Methods' => ['OPTIONS', 'GET', 'POST'],
+  #         'Access-Control-Allow-Headers' => 'Content-Type' 
+  # json test
+end
+
 get '/eeg' do
-  content_type :json    
-  headers 'Access-Control-Allow-Origin' => '*', 
-          'Access-Control-Allow-Methods' => ['OPTIONS', 'GET', 'POST'],
-          'Access-Control-Allow-Headers' => 'Content-Type' 
-  json test
+  work do
+    on neurosky, :eeg => :handle_feed
+  end
+end
+
+get '/attention' do
+  work do
+    on neurosky, :attention => :handle_feed
+  end
+end
+
+get '/meditation' do
+  work do
+    on neurosky, :meditation => :handle_feed
+  end
 end
