84a4b7a146f164ddc51fa8da70dde5b33c2169cf
Jack Bush <bush.jack@gmail.com>
2015-03-25 20:55:05 +0000

    added http 200 responses to API routes, enabled json gem, added note of which output is being sent

--- main.rb
+++ main.rb
@@ -1,7 +1,7 @@
 require 'sinatra'
 # require 'sinatra/contrib/all'
 # require 'sinatra/json'
-# require 'json'
+require 'json'
 # require 'pry-byebug'
 # require 'pry'
 require 'artoo'
@@ -10,25 +10,13 @@ connection :neurosky, :adaptor => :neurosky, :port => '/dev/tty.MindWave'
 device :neurosky, :driver => :neurosky, :interval => 0.1
 
 def handle_feed(sender, data)
-  puts sender
-  puts data
+  puts "#{sender} | #{data}"
 end
-
-# eeg = [
-#   # the below creates a feed of brain value hashes
-#   work do
-#     on neurosky, :eeg => :handle_feed
-#   end
-# ]
 
 test = [{thing: 4}]
 
 get '/' do
   erb :index
-end
-
-options '/eeg' do
-  200
 end
 
 get '/json' do
@@ -39,18 +27,27 @@ get '/json' do
   # json test
 end
 
+options '/eeg' do
+  200
+end
 get '/eeg' do
   work do
     on neurosky, :eeg => :handle_feed
   end
 end
 
+options '/attention' do
+  200
+end
 get '/attention' do
   work do
     on neurosky, :attention => :handle_feed
   end
 end
 
+options '/meditation' do
+  200
+end
 get '/meditation' do
   work do
     on neurosky, :meditation => :handle_feed
