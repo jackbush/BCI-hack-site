539de302a2c5851bb8d36af291d57ee3e78af3f8
Jack Bush <bush.jack@gmail.com>
2015-03-26 10:51:31 +0000

    added shotgun as doesn't clash with artoo. wrote in db config and load modules

--- main.rb
+++ main.rb
@@ -2,12 +2,24 @@ require 'sinatra'
 require 'artoo'
 require 'mongoid'
 require 'json' # POSSIBLY NOT NEEDED
+require 'shotgun'
 
 # GEMS INCOMPATIBLE WITH ARTOO
+# (sinatra?)
 # require 'sinatra/contrib/all'
 # require 'sinatra/json'
 # require 'pry-byebug'
 # require 'pry'
+
+# # DB CONFIG
+# configure do
+#   Mongoid.load!('./config/mongoid.yml')
+# end
+
+# # LOAD MODELS
+# before do
+#   @models = Model
+# end
 
 connection :neurosky, :adaptor => :neurosky, :port => '/dev/tty.MindWave'
 device :neurosky, :driver => :neurosky, :interval => 0.1
@@ -24,25 +36,6 @@ test_num = test_range.sample
 get '/' do
   erb :index
 end
-
-# SAMPLE JSON ROUTE
-get '/json' do
-  # content_type :json    
-  # headers 'Access-Control-Allow-Origin' => '*', 
-  #         'Access-Control-Allow-Methods' => ['OPTIONS', 'GET', 'POST'],
-  #         'Access-Control-Allow-Headers' => 'Content-Type' 
-  # json test
-end
-
-# MIGHT-WORK REFACTORING OF THE BELOW
-# options '/API/:type' do
-#   200
-# end
-# get '/API/:type' do
-#   work do
-#     on neurosky, :type => :handle_eeg
-#   end
-# end
 
 options '/eeg' do
   200
@@ -72,3 +65,22 @@ get '/meditation' do
     on neurosky, :meditation => :handle_eeg
   end
 end
+
+# SAMPLE JSON ROUTE
+# get '/json' do
+#   content_type :json    
+#   headers 'Access-Control-Allow-Origin' => '*', 
+#           'Access-Control-Allow-Methods' => ['OPTIONS', 'GET', 'POST'],
+#           'Access-Control-Allow-Headers' => 'Content-Type' 
+#   json test
+# end
+
+# MIGHT-WORK REFACTORING OF THE ABOVE
+# options '/API/:type' do
+#   200
+# end
+# get '/API/:type' do
+#   work do
+#     on neurosky, :type => :handle_eeg
+#   end
+# end
