a9a6c608276081c172882670eae3777c9ce96c04
Jack Bush <bush.jack@gmail.com>
2015-03-29 22:19:40 +0100

    eeg feed moved over, jquery stand-in cleared out

--- public/modules/visualisation/controllers/visualisation.client.controller.js
+++ public/modules/visualisation/controllers/visualisation.client.controller.js
@@ -2,27 +2,30 @@
 
 angular.module('visualisation').controller('VisualisationController', ['$scope',
 	function($scope) {
-		// processing inclusion
 
     var socket = io();
-    socket.on('connect', function() {
-      console.log('EEG SOCKET CONNECTED')
-    });
+
+    // FOR TESTING CONNECTION
+    // socket.on('connect', function() {
+    //   console.log('EEG SOCKET CONNECTED');
+    // });
+
     socket.on('eeg', function(data) {
       console.log(data);
-      $scope.eegBlink = data['blinkStrength'];
-      $scope.eegAttention = data['eSense']['attention']);
-      $scope.eegMeditation = data['eSense']['meditation']);
-      $scope.eegDelta = data['eegPower']['delta']);
-      $scope.eegTheta = data['eegPower']['theta']);
-      $scope.eegLowAlpha = data['eegPower']['lowAlpha']);
-      $scope.eegHighAlpha = data['eegPower']['highAlpha']);
-      $scope.eegLowBeta = data['eegPower']['lowBeta']);
-      $scope.eegHighBeta = data['eegPower']['highBeta']);
-      $scope.eegLowGamma = data['eegPower']['lowGamma']);
-      $scope.eegHighGamma = data['eegPower']['highGamma']);
+      $scope.eegSignal = data.poorSignalLevel;
+      $scope.eegBlink = data.blinkStrength;
+      $scope.eegAttention = data.eSense.attention;
+      $scope.eegMeditation = data.eSense.meditation;
+      $scope.eegDelta = data.eegPower.delta;
+      $scope.eegTheta = data.eegPower.theta;
+      $scope.eegLowAlpha = data.eegPower.lowAlpha;
+      $scope.eegHighAlpha = data.eegPower.highAlpha;
+      $scope.eegLowBeta = data.eegPower.lowBeta;
+      $scope.eegHighBeta = data.eegPower.highBeta;
+      $scope.eegLowGamma = data.eegPower.lowGamma;
+      $scope.eegHighGamma = data.eegPower.highGamma;
       // $scope.eegBlink = '0'
-      $scope.eegSignal = 100 - (data['poorSignalLevel']) / 2;
-    })
+      // $scope.eegSignal = 100 - (data.poorSignalLevel) / 2;
+    });
   }
 ]);
