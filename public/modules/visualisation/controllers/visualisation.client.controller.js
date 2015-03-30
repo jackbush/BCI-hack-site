b255d41cb55f5ed91c13a1fb7da253a144c35af5
Jack Bush <bush.jack@gmail.com>
2015-03-30 16:07:47 +0100

    socket moved to factory, passing scope successfully

--- public/modules/visualisation/controllers/visualisation.client.controller.js
+++ public/modules/visualisation/controllers/visualisation.client.controller.js
@@ -1,7 +1,9 @@
 'use strict';
 
-angular.module('visualisation').controller('VisualisationController', ['$scope',
-	function($scope) {
+angular.module('visualisation').controller('VisualisationController', ['$scope', 'socketFactory',
+	function($scope, socketFactory) {
+
+    // $scope.test = 'jack'
 
     var socket = io.connect();
 
@@ -10,11 +12,11 @@ angular.module('visualisation').controller('VisualisationController', ['$scope',
       console.log('EEG SOCKET CONNECTED');
     });
 
-    // FOR QUERYING SOCKET
-    socket.on('eeg', function(data) {
-      // $scope.test = 'hello world';
-      console.log(data.eSense.attention);
-      $scope.eegSignal = data.poorSignalLevel;
+    // console.log(socketFactory)
+
+    socketFactory().on('eeg', function(data) {
+      // if (data.keys) {};
+      console.log(data);
       $scope.eegBlink = data.blinkStrength;
       $scope.eegAttention = data.eSense.attention;
       $scope.eegMeditation = data.eSense.meditation;
@@ -26,6 +28,32 @@ angular.module('visualisation').controller('VisualisationController', ['$scope',
       $scope.eegHighBeta = data.eegPower.highBeta;
       $scope.eegLowGamma = data.eegPower.lowGamma;
       $scope.eegHighGamma = data.eegPower.highGamma;
+      $scope.eegSignal = (100 - (data.poorSignalLevel) / 2);
+
+    });
+
+    $scope.$on('socket:connect', function (ev, data) {
+      console.log('hi')
+      console.log(ev)
+      console.log(data)
+    })
+
+    // FOR QUERYING SOCKET
+    socket.on('eeg', function(data) {
+      // $scope.test = 'hello world';
+      // console.log(data);
+      // $scope.eegSignal = data.poorSignalLevel;
+      // $scope.eegBlink = data.blinkStrength;
+      // $scope.eegAttention = data.eSense.attention;
+      // $scope.eegMeditation = data.eSense.meditation;
+      // $scope.eegDelta = data.eegPower.delta;
+      // $scope.eegTheta = data.eegPower.theta;
+      // $scope.eegLowAlpha = data.eegPower.lowAlpha;
+      // $scope.eegHighAlpha = data.eegPower.highAlpha;
+      // $scope.eegLowBeta = data.eegPower.lowBeta;
+      // $scope.eegHighBeta = data.eegPower.highBeta;
+      // $scope.eegLowGamma = data.eegPower.lowGamma;
+      // $scope.eegHighGamma = data.eegPower.highGamma;
       // $scope.eegBlink = '0'
       // $scope.eegSignal = 100 - (data.poorSignalLevel) / 2;
     });
