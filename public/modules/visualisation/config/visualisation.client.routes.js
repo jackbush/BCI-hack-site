ac181f114ace2cbaf416664df4f437de51d34b36
Jack Bush <bush.jack@gmail.com>
2015-04-01 00:51:03 +0100

    cleaned up visualisations module

--- public/modules/visualisation/config/visualisation.client.routes.js
+++ public/modules/visualisation/config/visualisation.client.routes.js
@@ -2,12 +2,16 @@
 angular.module('visualisation').config(['$stateProvider',
 	function($stateProvider) {
 		$stateProvider
-    .state('visualisation', {
-			url: '/visualisation',
+    .state('visualisations', {
+			url: '/visualisations',
 			templateUrl: 'modules/visualisation/views/visualisation.client.view.html'
 		})
+    .state('circles', {
+      url: '/visualisations/circles',
+      templateUrl: 'modules/visualisation/views/circles.client.view.html'
+    })
     .state('waves', {
-      url: '/visualisation/waves',
+      url: '/visualisations/waves',
       templateUrl: 'modules/visualisation/views/waves.client.view.html'
     });
 	}
