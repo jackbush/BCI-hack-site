fe4fa54a8befbc1fe7ccc5f16ce1040f39ea60de
Jack Bush <bush.jack@gmail.com>
2015-03-31 09:31:37 +0100

    moved sketch into factory

--- public/modules/visualisation/services/eegvisualisation.client.service.js
+++ public/modules/visualisation/services/eegvisualisation.client.service.js
@@ -1,15 +1,19 @@
 'use strict';
 
-angular.module('visualisation').factory('Eegvisualisation', [
-	function() {
-		// Eegvisualisation service logic
-		// ...
+angular.module('visualisation').factory('Eegvisualisation', ['p5'
+	function(p5) {
+		return function(p) {
+	    var x = 0;
 
-		// Public API
-		return {
-			someMethod: function() {
-				return true;
-			}
-		};
+	    p.setup = function() {
+	      var eegCanvas = p.createCanvas(600, 400);
+			  p.background(200);
+	    };
+
+	    p.draw = function() {
+	      p.ellipse(x, height/2, 20, 20);
+			  x = x + 1;
+	    };
+	  };
 	}
 ]);
