a66d9a3b84f2170888591c9491cef3b5440973fa
Jack Bush <bush.jack@gmail.com>
2015-03-31 21:30:34 +0100

    moved eeg readout to header module

--- public/modules/core/controllers/header.client.controller.js
+++ public/modules/core/controllers/header.client.controller.js
@@ -1,7 +1,26 @@
 'use strict';
 
-angular.module('core').controller('HeaderController', ['$scope', 'Authentication', 'Menus',
-	function($scope, Authentication, Menus) {
-		// socket logic will go here
-	}
+angular.module('core').controller('HeaderController', ['$scope', 'socketFactory',
+  function($scope, socketFactory) {
+    // FOR TESTING CONNECTION
+    // var socket = io.connect();
+    // socket.on('connect', function() {
+    //   console.log('EEG SOCKET CONNECTED');
+    // });
+    socketFactory().on('eeg', function(data) {
+      // console.log(data);
+      $scope.eegBlink = data.blinkStrength;
+      $scope.eegAttention = data.eSense.attention;
+      $scope.eegMeditation = data.eSense.meditation;
+      $scope.eegDelta = data.eegPower.delta;
+      $scope.eegTheta = data.eegPower.theta;
+      $scope.eegLowAlpha = data.eegPower.lowAlpha;
+      $scope.eegHighAlpha = data.eegPower.highAlpha;
+      $scope.eegLowBeta = data.eegPower.lowBeta;
+      $scope.eegHighBeta = data.eegPower.highBeta;
+      $scope.eegLowGamma = data.eegPower.lowGamma;
+      $scope.eegHighGamma = data.eegPower.highGamma;
+      $scope.eegSignal = (100 - (data.poorSignalLevel) / 2);
+    });
+  }
 ]);
