42c09b12ee7674fb8ed38046af1a12062245817a
Jack Bush <bush.jack@gmail.com>
2015-03-31 11:48:22 +0100

    accessing socket inside p5 sketch

--- public/modules/visualisation/services/eegvisualisation.client.service.js
+++ public/modules/visualisation/services/eegvisualisation.client.service.js
@@ -4,14 +4,18 @@ angular.module('visualisation').factory('Eegvisualisation', ['p5',
 	function(p5) {
 		return function(p) {
 	    var x = 0;
+	    var socket = io.connect();
 
 	    p.setup = function() {
-	      var eegCanvas = p.createCanvas(600, 400);
-			  p.background(200);
+	      var eegCanvas = p.createCanvas(600, 500);
+			  p.background(238);
+		    socket.on('connect', function() {
+		      console.log('VIS SOCKET CONNECTED');
+		    });
 	    };
 
 	    p.draw = function() {
-	      p.ellipse(x, p.height/2, 20, 20);
+	      p.ellipse(x, p.height/2, x/3, x/3);
 			  x = x + 1;
 	    };
 	  };
