2b43c7a011d16877c47fc2faae32941cfd6b6bc9
Jack Bush <bush.jack@gmail.com>
2015-04-01 23:12:45 +0100

    circles basics working

--- public/modules/core/services/introsketch.client.service.js
+++ public/modules/core/services/introsketch.client.service.js
@@ -3,47 +3,37 @@
 angular.module('core').factory('introSketch', [
 	function(p5) {
 		return function(p) {
-	    p.colorMode(p.RGB, 255);
+	    p.colorMode(p.RGBA, 255);
 	    var socket = io.connect();
-	    var vertC = window.innerHeight/2;
-	    var horiC = window.innerWidth/2;
-	    var white = p.color(255,255,255);
-	    var navy = p.color(20,24,39);
+	    var p5height = window.innerHeight;
+	    var p5width = window.innerWidth;
+	    var white = p.color(255,255,255,255);
+	    var navy = p.color(20,24,39,255);
 	    var xpos = 0;
 
 	    p.setup = function() {
 	      p.createCanvas(window.innerWidth, window.innerHeight);
 			  p.background(white);
 	    };
-	    
+
 	    p.draw = function() {
-	    	// console.log('hi')
-			  p.background(white);
-			  p.fill(navy);
-			  p.stroke(0,0,0);
-			  p.ellipseMode(p.CENTER);
-			  xpos = xpos + 1;
-			  if(xpos > p.width) {
-			  	xpos = 0;
-			  }
-			  // draw a circle
-			  p.ellipse(xpos, 100, 25, 25);
-			  // display xpos variable
-			  p.fill(0);
-			  p.text("xpos = " + xpos, 25, 25);
+			  socket.on('eeg', function(data) {
+				  var attention = (data.eSense) ? data.eSense.attention : attention;
+				  var meditation = (data.eSense) ? data.eSense.meditation : meditation;
+					// console.log(attention);
+				  p.background(255,255,255,30);
+				  p.fill(150,250,150,50);
+				  p.noStroke();
+				  p.ellipseMode(p.CENTER);
+				  xpos = xpos + 1;
+				  if(xpos > p.width) {
+				  	xpos = 0;
+				  }
+				  // draw circles
+				  p.ellipse(p5width/2+3*attention, p5height/2+25, 0.8*p5height, 0.8*p5height);
+				  p.ellipse(p5width/2-3*attention, p5height/2+25, 0.8*p5height, 0.8*p5height);
+			  });
 	    };
 	  };
 	}
 ]);
-
-// socket.on('eeg', function(data) {
-//   var attention = data.eSense.attention;
-//   var meditation = data.eSense.meditation;
-//   // p.background(128);
-//   p.noFill();
-//   p.stroke(255);
-//   p.strokeWeight(1);
-//   p.ellipse(0, window.innerHeight/2, 18*attention,18*attention);
-//   p.stroke(0);
-//   p.ellipse(window.innerWidth, window.innerHeight/2, 18*meditation,18*meditation);
-// });
