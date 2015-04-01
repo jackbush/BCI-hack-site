0538a4d966fb9450400480ecb4299489fbbd5a6c
Jack Bush <bush.jack@gmail.com>
2015-04-01 17:05:01 +0100

    consolidated all full page sketch styles to one class in core. prepared and inserted sketches to remaining pages

--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -1,27 +1,23 @@
 'use strict';
 
-angular.module('core').controller('HomeController', ['$scope',
-	function($scope) {
+angular.module('core').controller('HomeController', ['$scope', 'socketFactory',
+  function($scope, socketFactory) {
     $scope.selectedIntroduction = '1';
     $scope.introductions = [{
       num: '1',
       title: 'Welcome',
-      sketch: 'sketch',
       body: 'Below is a brief connection guide and two simple exercises to introduce you to the technology. Once you feel comfortable, use the navigation links above to click through to more complex games and visualisations. Using the full site requires an EEG headset.'
     },{
       num: '2',
       title: 'Connecting',
-      sketch: 'sketch',
       body: 'To get started, plug in your headset and click the CONNECTION area in the top right corner. As soon as a link is established, streaming data will appear. This panel is available throughout the site.'
     },{
       num: '3',
       title: 'Focus',
-      sketch: 'sketch',
       body: 'Control the size of the circle by focussing on it. See if you can get it to touch the edge of the canvas, and how long you can hold it at that size.'
     },{
       num: '4',
       title: 'Meditation',
-      sketch: 'sketch',
       body: 'Calm your mind to slow the oscillation of the graph. Breathe deeply and see if you can get the visualisation to stop entirely.'
     }];
     $scope.selectIntroduction = function(introduction) {
