f740b785446a923c17c4ec0cfdf1c88286df374a
Jack Bush <bush.jack@gmail.com>
2015-04-03 22:53:03 +0100

    ready for heroku deployment

--- server.js
+++ server.js
@@ -21,25 +21,25 @@ var db = mongoose.connect(config.db, function(err) {
 // init express application
 var app = require('./config/express')(db);
 
-// EEG CONNECTION (does not prevent error when thinkgear driver is not on)
-try {
-  // INIT
-  var client = neurosky.createClient({
-    appName:'NodeNeuroSky',
-    appKey:'0fc4141b4b45c675cc8d3a765b8d71c5bde9390'
-  });
-  // CONNECTION
-  client.connect();
+// // EEG CONNECTION (does not prevent error when thinkgear driver is not on)
+// try {
+//   // INIT
+//   var client = neurosky.createClient({
+//     appName:'NodeNeuroSky',
+//     appKey:'0fc4141b4b45c675cc8d3a765b8d71c5bde9390'
+//   });
+//   // CONNECTION
+//   client.connect();
   var io = app.get('socketio');
   // SOCKET EMIT
   io.on('connect', function(socket) {
-    client.on('data',function(data) {
-      socket.emit('eeg', data);
-    });
+    // client.on('data',function(data) {
+      socket.emit('eeg', 'data');
+    // });
   });
-} catch(e) {
-  console.log(e);
-}
+// } catch(e) {
+//   console.log(e);
+// }
 
 // Bootstrap passport config
 require('./config/passport')();
