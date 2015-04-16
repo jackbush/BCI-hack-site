5b325b8a3648eec755143dff1d280c695da1d2a9
Jack Bush <bush.jack@gmail.com>
2015-04-16 16:24:37 +0100

    site no longer crashes on nav

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ var init = require('./config/init')(),
 	config = require('./config/config'),
 	mongoose = require('mongoose'),
 	chalk = require('chalk');
-  // neurosky = require('node-neurosky');
 
 // MAIN APPLICATION ENTRY FILE
 
@@ -21,25 +20,14 @@ var db = mongoose.connect(config.db, function(err) {
 // init express application
 var app = require('./config/express')(db);
 
-// // EEG CONNECTION (does not prevent error when thinkgear driver is not on)
-// try {
-//   // INIT
-//   var client = neurosky.createClient({
-//     appName:'NodeNeuroSky',
-//     appKey:'0fc4141b4b45c675cc8d3a765b8d71c5bde9390'
-//   });
-//   // CONNECTION
-//   client.connect();
-  var io = app.get('socketio');
-  // SOCKET EMIT
-  io.on('connect', function(socket) {
-    // client.on('data',function(data) {
-      socket.emit('eeg', 'data');
-    // });
-  });
-// } catch(e) {
-//   console.log(e);
-// }
+// init socket
+// THIS IS A DEMO BRANCH. FOR EEG INJECTION, CHECK OUT ANY OTHER BRANCH.
+var io = app.get('socketio');
+io.on('connect', function(socket) {
+  setInterval(function() { 
+    socket.emit('eeg', 'start');
+  }, 500);
+});
 
 // Bootstrap passport config
 require('./config/passport')();
