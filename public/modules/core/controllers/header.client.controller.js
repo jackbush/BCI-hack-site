f77849500f7c7be7926d88bda7a6f73efb9addec
Jack Bush <bush.jack@gmail.com>
2015-04-18 14:32:15 +0100

    migrated production version to use local server

--- public/modules/core/controllers/header.client.controller.js
+++ public/modules/core/controllers/header.client.controller.js
@@ -6,18 +6,12 @@ angular.module('core').controller('HeaderController', ['$scope', '$window', 'soc
     $scope.toggleEeg = function() {
         $scope.check = $scope.check === false ? true : false;
     };
-    // FOR TESTING CONNECTION
-    // var socket = io.connect();
-    // socket.on('connect', function() {
-    //   console.log('EEG SOCKET CONNECTED');
-    // });
+
     try {
       var socket = io.connect('http://localhost:9876');
-    }
-    catch(e) {
-      console.log(e)
+    } catch(e) {
       var socket = io.connect();
-    }
+    };
     socket.on('eeg', function(data) {
       console.log(data);
       $scope.eegBlink = (data) ? (data.blinkStrength) : '';
@@ -33,20 +27,5 @@ angular.module('core').controller('HeaderController', ['$scope', '$window', 'soc
       $scope.eegHighGamma = (data) ? (data.eegPower.highGamma) : '';
       $scope.eegSignal = (100 - (data.poorSignalLevel) / 2);
     });
-    // socketFactory().on('eeg', function(data) {
-    //   console.log(data);
-    //   $scope.eegBlink = (data) ? (data.blinkStrength) : '';
-    //   $scope.eegAttention = (data) ? (data.eSense.attention) : '';
-    //   $scope.eegMeditation = (data) ? (data.eSense.meditation) : '';
-    //   $scope.eegDelta = (data) ? (data.eegPower.delta) : '';
-    //   $scope.eegTheta = (data) ? (data.eegPower.theta) : '';
-    //   $scope.eegLowAlpha = (data) ? (data.eegPower.lowAlpha) : '';
-    //   $scope.eegHighAlpha = (data) ? (data.eegPower.highAlpha) : '';
-    //   $scope.eegLowBeta = (data) ? (data.eegPower.lowBeta) : '';
-    //   $scope.eegHighBeta = (data) ? (data.eegPower.highBeta) : '';
-    //   $scope.eegLowGamma = (data) ? (data.eegPower.lowGamma) : '';
-    //   $scope.eegHighGamma = (data) ? (data.eegPower.highGamma) : '';
-    //   $scope.eegSignal = (100 - (data.poorSignalLevel) / 2);
-    // });
   }
 ]);
