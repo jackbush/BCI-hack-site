66e83cacb13e25c3e503ecc466af700c1d3a926d
Jack Bush <bush.jack@gmail.com>
2015-04-19 22:15:48 +0100

    grunt build with apply binding for prodution

--- public/dist/application.js
+++ public/dist/application.js
@@ -88,26 +88,24 @@ angular.module('core').controller('HeaderController', ['$scope', '$window', 'soc
     $scope.toggleEeg = function() {
       $scope.check = $scope.check === false ? true : false;
     };
-
-    try {
-      var socket = io.connect('http://localhost:9876');
-    } catch(e) {
-      var socket = io.connect();
-    };
+    
+    var socket = io.connect('http://localhost:9876');
     socket.on('eeg', function(data) {
       console.log(data);
-      $scope.eegBlink = (data) ? (data.blinkStrength) : '';
-      $scope.eegAttention = (data) ? (data.eSense.attention) : '';
-      $scope.eegMeditation = (data) ? (data.eSense.meditation) : '';
-      $scope.eegDelta = (data) ? (data.eegPower.delta) : '';
-      $scope.eegTheta = (data) ? (data.eegPower.theta) : '';
-      $scope.eegLowAlpha = (data) ? (data.eegPower.lowAlpha) : '';
-      $scope.eegHighAlpha = (data) ? (data.eegPower.highAlpha) : '';
-      $scope.eegLowBeta = (data) ? (data.eegPower.lowBeta) : '';
-      $scope.eegHighBeta = (data) ? (data.eegPower.highBeta) : '';
-      $scope.eegLowGamma = (data) ? (data.eegPower.lowGamma) : '';
-      $scope.eegHighGamma = (data) ? (data.eegPower.highGamma) : '';
-      $scope.eegSignal = (100 - (data.poorSignalLevel) / 2);
+      $scope.$apply(function() {
+        $scope.eegBlink = (data) ? (data.blinkStrength) : '';
+        $scope.eegAttention = (data) ? (data.eSense.attention) : '';
+        $scope.eegMeditation = (data) ? (data.eSense.meditation) : '';
+        $scope.eegDelta = (data) ? (data.eegPower.delta) : '';
+        $scope.eegTheta = (data) ? (data.eegPower.theta) : '';
+        $scope.eegLowAlpha = (data) ? (data.eegPower.lowAlpha) : '';
+        $scope.eegHighAlpha = (data) ? (data.eegPower.highAlpha) : '';
+        $scope.eegLowBeta = (data) ? (data.eegPower.lowBeta) : '';
+        $scope.eegHighBeta = (data) ? (data.eegPower.highBeta) : '';
+        $scope.eegLowGamma = (data) ? (data.eegPower.lowGamma) : '';
+        $scope.eegHighGamma = (data) ? (data.eegPower.highGamma) : '';
+        $scope.eegSignal = (100 - (data.poorSignalLevel) / 2);
+      });
     });
   }
 ]);
